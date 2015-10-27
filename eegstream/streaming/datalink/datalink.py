7f707c0d9285a14f83926f84850fc0cb0ab88dcc
Egor Krivov <egor.krivov@frtk.ru>
2015-10-27 20:06:08 +0300

    abstract class for data link

---
+++ eegstream/streaming/datalink/datalink.py
@@ -0,0 +1,46 @@
+from abc import ABCMeta, abstractmethod
+
+
+def take_docs(other_func):
+    def dec(func):
+        func.__doc__ = other_func.__doc__
+        return func
+    return dec
+
+
+class DataLinkTransmitter(metaclass=ABCMeta):
+    """Class, supporting bytes transmission via some data link."""
+    @abstractmethod
+    def __enter__(self):
+        """Initialization process."""
+        return
+
+    @abstractmethod
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Close process."""
+        return
+
+    @abstractmethod
+    def send(self, bytes):
+        """Send bytes via data link."""
+        return
+
+
+class DataLinkReceiver(metaclass=ABCMeta):
+    """Class, supporting bytes receiving via some data link."""
+    @take_docs(DataLinkTransmitter.__enter__)
+    @abstractmethod
+    def __enter__(self):
+        """Initialization process."""
+        return
+
+    @take_docs(DataLinkTransmitter.__exit__)
+    @abstractmethod
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Close process."""
+        return
+
+    @abstractmethod
+    def receive(self, bytes):
+        """Receive bytes via data link."""
+        return
